1d0467c7ae10186e6d74d0b51af895524470f234
A.STEPHENRAJ <144765745+C22UG154CSC023@users.noreply.github.com>
2026-03-04 21:22:10 +0530

    Add files via upload

---
+++ Oops concept.py
@@ -0,0 +1,48 @@
+from enum import nonmember
+"""class students:
+    Name = None
+    Register_number = None
+    standard = None
+    phone_number = None
+    def __init__(self, Name, Register_number, standard , phone_number):
+        self.Name = Name
+        self.Register_number = Register_number
+        self.standard = standard
+        self.phone_number = phone_number
+        print("Name:", self.Name)
+        print("Register_number:", self.Register_number)
+        print("standard:", self.standard)
+        print("phone_number:", self.phone_number)
+a = students("Stephenraj","C22UG154CSC023","Final year","8838260257")
+"""
+
+"""class Bike:
+    brand = None
+    model = None
+    color = None
+    price = None
+    fuel = None
+    def Bike(self):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.price = price
+        self.fuel = fuel
+        print("brand:", self.brand)
+        print("model:", self.model)
+        print("color:", self.color)
+        print("price:", self.price)
+        print("fuel:", self.fuel)
+        print("END".center(25,"*"))
+B = Bike()
+B.brand="Pulsar"
+B.model="Ns"
+B.fuel="petrol"
+B.color="Red"
+B.price=135000
+print("brand :",B.brand)
+print("model :",B.model)
+print("color :",B.color)
+print("price :",B.price)
+print("fuel :",B.fuel)
+"""
