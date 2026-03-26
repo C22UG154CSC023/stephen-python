b7076ced3f895b2a8385cae355b4fbe1c6048824
A.STEPHENRAJ <144765745+C22UG154CSC023@users.noreply.github.com>
2026-03-26 23:23:38 +0530

    Add files via upload

---
+++ intro sql.sql
@@ -0,0 +1,9 @@
+create database arun;
+use arun;
+create table loan(name varchar(20),Deposit int, Balance int);
+insert into loan values('stephen',25000,25000);
+select*from loan;
+select name from loan;
+select Deposit from loan;
+select Balance from loan;
+select * from loan;
